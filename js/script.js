8098a3e0c5079e7ab4964e4afd823dcb363944f4
SamaraBilby <samarabilby@hotmail.com>
2022-07-17 00:56:27 -0400

    iniciando html área de login  e lógica js de ocultar e mostrar senha

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let senha = document.getElementById('senha');
+
+document.getElementById('senha-ocultar').addEventListener('click', ()=> {
+    if(senha.type == "password") {
+        senha.focus()
+    document.getElementById('senha-ocultar').style.display = "none";
+    document.getElementById('senha-mostrar').style.display = "inline-block";
+        return senha.type = "text";
+    } else {
+        return senha.type = "password";
+    }
+});
+
+document.getElementById('senha-mostrar').addEventListener('click', ()=> {
+    if(senha.type === "text") {
+        senha.focus()
+    document.getElementById('senha-mostrar').style.display = "none";
+    document.getElementById('senha-ocultar').style.display = "inline-block";
+        return senha.type = "password";
+    } else {
+        return senha.type = "text";
+    }
+    
+});
