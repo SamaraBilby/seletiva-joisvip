2d4384039d4ba6a37761bc50e88ab6f922095830
SamaraBilby <samarabilby@hotmail.com>
2022-07-18 13:40:35 -0400

    finalização da lógica de login

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 
-let form = document.querySelector('form')
+const form = document.querySelector('#user-login');
 let senha = document.getElementById('senha');
-let email = document.getElementById('email')
+let email = document.getElementById('email');
 
 /* mostrar ou ocultar senha */
 
 document.getElementById('senha-ocultar').addEventListener('click', ()=> {
     if(senha.type == "password") {
         senha.focus()
-    document.getElementById('senha-ocultar').style.display = "none";
-    document.getElementById('senha-mostrar').style.display = "inline-block";
+        document.getElementById('senha-ocultar').style.display = "none";
+        document.getElementById('senha-mostrar').style.display = "inline-block";
         return senha.type = "text";
     } else {
         return senha.type = "password";
@@ -19,8 +19,8 @@ document.getElementById('senha-ocultar').addEventListener('click', ()=> {
 document.getElementById('senha-mostrar').addEventListener('click', ()=> {
     if(senha.type === "text") {
         senha.focus()
-    document.getElementById('senha-mostrar').style.display = "none";
-    document.getElementById('senha-ocultar').style.display = "inline-block";
+        document.getElementById('senha-mostrar').style.display = "none";
+        document.getElementById('senha-ocultar').style.display = "inline-block";
         return senha.type = "password";
     } else {
         return senha.type = "text";
@@ -28,7 +28,41 @@ document.getElementById('senha-mostrar').addEventListener('click', ()=> {
     
 });
 
+
+
+/* mensagem de err0 */
+function criaDivErroLogin(){
+    const erroLogin = document.createElement('div');
+    erroLogin.classList.add("alerta-erro-login");
+    erroLogin.textContent = "Usuário ou senha incorretos"
+    divMensagemErro = document.querySelector("#erro-login");
+
+    divMensagemErro.appendChild(erroLogin);
+};
+
 /* validação login */
 
-
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
     
+    function logar() {
+        
+        let usuario = email.value;
+        let password = senha.value;
+        
+        let apagarDivMensagemErro = document.querySelector("#erro-login");
+        apagarDivMensagemErro.textContent = "";
+
+
+        if(usuario == 'admin@joiasvip.com' && password == "123@admin"){
+            alert('Sucesso');
+            location.href ="produtos.html"
+        } 
+        else {
+            criaDivErroLogin();
+        }
+    }
+    
+    logar()  
+})
+
